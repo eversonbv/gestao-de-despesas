357a7fac6b57938deb595514be3fa714fa245a28
Everson Borges Volaco <everson.volaco@gmail.com>
2022-08-16 08:34:25 -0300

    Atividade 07 para o develop

--- gestao-despesas/src/app/app.module.ts
+++ gestao-despesas/src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Componente1Component } from './componente1/componente1.component';
-import { Componente2Component } from './componente2/componente2.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Componente1Component,
-    Componente2Component
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
