36a62d3f9bf027371fcf44978db16f2a6edbf253
Everson Borges Volaco <everson.volaco@gmail.com>
2022-08-17 14:01:22 -0300

    Atividade 08

--- gestao-despesas/src/app/footer/footer.component.ts
+++ gestao-despesas/src/app/footer/footer.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  public ano : number;
+  constructor() {
+    this.ano = new Date().getFullYear();
+  }
 
   ngOnInit(): void {
   }
---
+++ gestao-despesas/src/app/land-page/land-page.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-land-page',
+  templateUrl: './land-page.component.html',
+  styleUrls: ['./land-page.component.css'],
+})
+export class LandPageComponent implements OnInit {
+  public isHidePanel = false;
+  public imageURL : string = 'https://placeimg.com/600/300/tech';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  getBackgroundImage() {
+    return {
+      'background-image':
+        'linear-gradient(rgba(0, 0, 0, .7), rgba(0, 0, 0, .9)), url(' +
+        this.imageURL +
+        ')',
+    };
+  }
+
+}
